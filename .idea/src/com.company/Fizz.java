64e9c29433438d194eb8e5ce10155e90f6ecc2f9
krzysztofjuszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2022-10-16 17:38:49 +0200

    fizzbuzz

--- .idea/src/com.company/Fizz.java
+++ .idea/src/com.company/Fizz.java
@@ -6,17 +6,18 @@ public class Fizz {
         System.out.println(fizzBuzz(test));
         fizzBuzz2();
     }
+
     //Write a method that returns 'Fizz' for multiples of three and 'Buzz' for the multiples of five.
     //For numbers which are multiples of both three and five return 'FizzBuzz'.
     //For numbers that are neither, return the input number.
     public static String fizzBuzz(int i) {
         String result = "";
         if (i % 15 == 0) {
-            return result="fizzbuzz";
+            return result = "fizzbuzz";
         } else if (i % 5 == 0) {
-            return result="buzz";
+            return result = "buzz";
         } else if (i % 3 == 0) {
-            return result="fizz";
+            return result = "fizz";
         }
         return String.valueOf(i);
 
@@ -31,6 +32,4 @@ public class Fizz {
                             : ((i % 5 == 0) ? "buzz " : i + " ")));
         }
     }
-
-
-}
+    }
